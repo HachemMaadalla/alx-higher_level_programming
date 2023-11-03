2608c8dac8d0b01e8688c4fa8058e5a383809c79
HachemMaadalla <138704463+HachemMaadalla@users.noreply.github.com>
2023-11-03 06:42:07 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-import sys
-count = len(sys.argv) - 1
-if count == 0:
-print("0 arguments.")
-elif count == 1:
-print("1 argument:")
-else:
-print("{} arguments:".format(count))
-for i in range(count):
-print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    from sys import argv
+    size = len(argv)
+    if size > 2:
+        print("{:d} arguments:".format(size - 1))
+    elif size == 1:
+        print("0 arguments.")
+    else:
+        print("1 argument:")
+    for i in range(1, size):
+        print("{:d}: {:s}".format(i, argv[i]))
