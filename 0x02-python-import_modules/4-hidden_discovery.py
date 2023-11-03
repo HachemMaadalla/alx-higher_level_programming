1c94f0058b8c2c0806eb5651dec2c4f5edd178d9
HachemMaadalla <138704463+HachemMaadalla@users.noreply.github.com>
2023-11-03 06:42:48 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-import hidden_4
-names = dir(hidden_4)
-for name in names:
-if name[:2] != "__":
-print(name)
+if __name__ == "__main__":
+    import hidden_4
+    defined = []
+    for w in dir(hidden_4):
+        if not w.startswith('__'):
+            defined.append(w)
+    defined.sort()
+    for w in defined:
+        print("{:s}".format(w))
