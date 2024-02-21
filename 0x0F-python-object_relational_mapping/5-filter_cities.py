1f1765fda3a227a2bc85cf2f04b66c1cc6cf01b8
HachemMaadalla <138704463+HachemMaadalla@users.noreply.github.com>
2024-02-21 02:23:06 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
-"""
-    Module that performs MySQL query through MySQLdb module.
-"""
-
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    try:
+        conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
 
-    cur = conn.cursor()
+        cur = conn.cursor()
+        stmt = "SELECT c.name FROM cities c INNER JOIN states s ON \
+                c.state_id = s.id WHERE s.name = %s ORDER BY c.id ASC"
+        cur.execute(stmt, (sys.argv[4],))
+        query_rows = cur.fetchall()
 
-    stmt = "SELECT c.name, s.name FROM cities c INNER JOIN states s ON \
-    c.state_id = s.id ORDER BY c.id"
-    cur.execute(stmt)
-    query_rows = cur.fetchall()
+        cities = [row[0] for row in query_rows]
+        print(', '.join(cities))
 
-    i = 0
-    for row in query_rows:
-        if row[1] == sys.argv[4]:
-            if i != 0:
-                print(', ', end='')
-            print(row[0], end='')
-            i += 1
-    print('')
-
-    cur.close()
-    conn.close()
+        cur.close()
+        conn.close()
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL: {e}")
