37511985ea2b1d0db116a73fb5de5435b6eae7b5
HachemMaadalla <138704463+HachemMaadalla@users.noreply.github.com>
2024-02-21 02:19:58 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
-"""
-    Module that performs MySQL query through MySQLdb module.
-"""
-
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    try:
+        conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+        
+        cur = conn.cursor()
+        cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+        query_rows = cur.fetchall()
 
-    cur = conn.cursor()
-    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
 
-    for row in query_rows:
-        print(row)
-
-    cur.close()
-    conn.close()
+        cur.close()
+        conn.close()
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL: {e}")
