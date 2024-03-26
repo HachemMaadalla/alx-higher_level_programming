2decc598cc1ad9bbf8b7b1f6ddd3991eb8cf70c0
HachemMaadalla <138704463+HachemMaadalla@users.noreply.github.com>
2024-03-26 20:32:43 +0000

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,23 @@
 #!/usr/bin/node
+// web scrapper
 
 const request = require('request');
 
-request(process.argv[2], function (err, resp, body) {
-  if (err) {
-    console.log(err);
-  } else if (resp.statusCode === 200) {
-    let users = {};
-    let todos = JSON.parse(body);
-    for (let i = 0; i < todos.length; i++) {
-      if (todos[i].completed === true) {
-        if (todos[i].userId in users) {
-          users[todos[i].userId]++;
-        } else {
-          users[todos[i].userId] = 1;
-        }
+request.get(process.argv[2], { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
       }
     }
-    console.log(users);
-  }
+  });
+  console.log(tasksCompleted);
 });
