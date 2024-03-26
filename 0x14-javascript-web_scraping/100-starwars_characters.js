974e006d5ef8492d12142c7741c1a2d878aaabc4
HachemMaadalla <138704463+HachemMaadalla@users.noreply.github.com>
2024-03-26 20:35:18 +0000

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,18 +1,23 @@
 #!/usr/bin/node
 
 const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request('https://swapi.co/api/films/' + process.argv[2], function (err, resp, body) {
-  if (err) {
-    console.log(err);
-  } else if (resp.statusCode === 200 && resp.headers['content-type'] === 'application/json') {
-    let chars = JSON.parse(body).characters;
-    for (let i = 0; i < chars.length; i++) {
-      request(chars[i], function (error, response, body) {
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const content = JSON.parse(body);
+    const characters = content.characters;
+    // console.log(characters);
+    for (const character of characters) {
+      request.get(character, (error, response, body) => {
         if (error) {
           console.log(error);
-        } else if (response.statusCode === 200) {
-          console.log(JSON.parse(body).name);
+        } else {
+          const names = JSON.parse(body);
+          console.log(names.name);
         }
       });
     }
